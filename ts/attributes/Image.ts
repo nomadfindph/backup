aac7f166aeeb06e69094a8b08dd93620efb791a3
nomadfindph <105588333+nomadfindph@users.noreply.github.com>
2022-06-09 02:01:54 +0800

    Delete ts directory

--- ts/attributes/Image.ts
+++
@@ -1,7 +0,0 @@
-export default class Image {
-    public url: URL;
-
-    constructor(url: URL){
-        this.url = url;
-    }
-}
--- ts/classes/User.ts
+++
@@ -1,6 +0,0 @@
-export default interface User {
-    name: string;
-    email: string;
-    username: string;
-    password: string;
-}
